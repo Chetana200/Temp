4c6656b981987ab8a2db989c6e52f024cf05919c
Chetana200 <agarwalchetana08@gmail.com>
2023-02-01 12:07:08 +0530

    function

---
+++ src/Calculator/add.c
@@ -0,0 +1,20 @@
+
+int addition(int num1,int num2)
+{
+    return num1+num2;
+}
+
+int subtraction(int num1,int num2)
+{
+    return num1-num2;
+}
+
+int multiplication(int num1,int num2)
+{
+    return num1*num2;
+}
+
+int divison(int num1 ,int num2)
+{
+    return num1/num2;
+}
